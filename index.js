5e00d094aeec1666a225715d6c5628ac35205403
Sayantan <this.sayantan@gmail.com>
2023-12-09 22:46:48 +0700

    🔧 refactor(index.js): simplify time property in buildMsg function to use raw Date object for better flexibility and simplicity

--- index.js
+++ index.js
@@ -123,11 +123,7 @@ function buildMsg(name, text) {
   return {
     name,
     text,
-    time: new Intl.DateTimeFormat("default", {
-      hour: "numeric",
-      minute: "numeric",
-      second: "numeric",
-    }).format(new Date()),
+    time: new Date(),
   };
 }
 
